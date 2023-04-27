8250a61a4fa0e6cd1abf87036dbf7a2e7a246540
Albina <olifirenko.albina@gmail.com>
2023-04-27 10:12:06 +0300

    Start new project delete storyboard

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var new data = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
